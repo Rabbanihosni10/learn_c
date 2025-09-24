2b477486f89917ea316237da8f455ddf6ee2e097
Rabbanihosni10 <rabbanihosni10@gmail.com>
2025-09-24 16:27:31 +0600

    04_05_06

---
+++ Chap1/05_comments.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    //Single line comment
+    // I am writing this just for fun. This line doesnt do anything
+
+    printf("Hello World!");
+
+    /* Multi line comment 
+    Program: 05_comment.c
+    author: Harry
+    date: 20-01-2024
+    */
+
+    return 0;
+}
